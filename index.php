c42460349729eb1301bde112dcecd382bcdbde1a
jeanne mariel mojica de peña <jeam1214@hotmail.com>
2021-07-26 12:11:39 -0400

    adding libraries

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda Telefonica</title>
+    <link rel="stylesheet" href="assets/css/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+   
+
+
+   
+   <script href="assets/js/bootstrap/js/bootstrap.min.js"></script>
+   <script href="assets/js/bootstrap/js/bootstrap.bundle.js"></script>
+   <script href="assets/js/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
